e1b15d8f94684be89856445839badaef90fdcd42
Joe Letner <jrletner@gmail.com>
2025-08-25 18:41:23 -0500

    updated lesson

---
+++ Lectures/JavaScript/8_25_25_Lecture/app.js
@@ -0,0 +1,48 @@
+const colors = ["red", "green", "blue"] // arrays are zero index [0]
+
+// For loop
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i])
+// }
+
+// Managing collections of data
+// const mixedArray = [
+// 1, "hello", true, { name: {first: "Joe",last: "Letner"} }, [10, 20]
+// ]
+// console.log(mixedArray);
+// console.log(Array.isArray(mixedArray));
+
+// colors[1] = "Yellow"
+// console.log(colors[1]);
+
+// const numbers = [1, 2, 3]
+// numbers.push(4)
+// console.log(numbers);
+// numbers.pop()
+// console.log(numbers);
+// numbers.shift()
+// console.log(numbers);
+// numbers.splice(0)
+// console.log(numbers);
+
+// While Loop
+// let count = 3
+// while (count > 0) {
+//     console.log(count);
+//     count--
+// }
+
+// for...of loop
+// const fruits = ["apple", "banana", "cherry"];
+// for (const fruit of fruits) {
+//     console.log(fruit);
+
+// }
+
+// for...in object
+// const user = { name: "Alice", age: 25, profession: "Developer" }
+// for (const key in user) {
+//     if (Object.hasOwnProperty.call(user, key)) {
+//         console.log(`${key}: ${user[key]}`);
+//     }
+// }
