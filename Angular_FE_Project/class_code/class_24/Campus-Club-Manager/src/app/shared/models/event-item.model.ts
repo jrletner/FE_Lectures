6c3049b7d8a082873518b911393b77cc9c53b43f
Joe Letner <jrletner@gmail.com>
2025-10-23 12:31:46 -0500

    Delete Angular_FE_Project/class_code directory

--- Angular_FE_Project/class_code/class_24/Campus-Club-Manager/src/app/shared/models/event-item.model.ts
+++
@@ -1,7 +0,0 @@
-export interface EventItem {
-  id: string;
-  title: string;
-  dateIso: string; // ISO date string
-  capacity: number;
-  description?: string;
-}
--- Angular_FE_Project/class_code/class_24/Campus-Club-Manager/src/app/shared/tokens/api-base.token.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const API_BASE = new InjectionToken<string>('API_BASE');
